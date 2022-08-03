7450eba49f40f9f87ae0d2aaa24d49e037909e03
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-08-03 12:58:22 -0700

    added 'create' and 'update' functionality to console

--- console.py
+++ console.py
@@ -3,11 +3,17 @@
 import cmd
 import sys
 import shlex
+import json
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
+
+    class_dict = {
+            "BaseModel": BaseModel
+            }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -23,20 +29,78 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Create a new instance of BaseModel, saves it to Json file"""
-        class_dict = {
-                "BaseModel": BaseModel
-                }
         arg_list = shlex.split(arg)
         if len(arg_list) < 1:
             print("** class name missing **")
             return
 
-        if (arg_list[0] in class_dict.keys()):
-            obj = class_dict[arg_list[0]]()
+        if (arg_list[0] in HBNBCommand.class_dict.keys()):
+            obj = HBNBCommand.class_dict[arg_list[0]]()
             obj.save()
             print(obj.id)
         else:
             print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """ Prints all string representation of all instances
+        based or not on the class name."""
+        storage.reload()
+        obj_list = []
+        objects = storage.all()
+        if arg == "":
+            for key, value in objects.items():
+                obj_list.append(str(value))
+            print(json.dumps(obj_list))
+            return
+        arg_list = shlex.split(arg)
+        if (arg_list[0] not in HBNBCommand.class_dict.keys()):
+            print("** class doesn't exist **")
+            return
+        for key, value in objects.items():
+            if arg_list[0] in key:
+                obj_list.append(str(value))
+            print(json.dumps(obj_list))
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file).
+        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com
+
+        update <class name> <id> <attribute name> "<attribute value>"
+
+        """
+        arg_list = shlex.split(arg)
+        arg_len = len(arg_list)
+        if arg_len < 1:
+            print("** class name missing **")
+            return
+        if arg_list[0] not in HBNBCommand.class_dict.keys():
+            print("** class doesn't exist **")
+            return
+        if arg_len == 1:
+            print("** instance id missing **")
+            return
+        storage.reload()
+        objects = storage.all()
+        key = arg_list[0] + "." + arg_list[1]
+        try:
+            objects[key]
+        except KeyError:
+            print("** no instance found **")
+            return
+        if arg_len == 2:
+            print("** attribute name missing **")
+            return
+        if arg_len == 3:
+            print("** value missing **")
+            return
+        obj_dict = objects[key].__dict__
+        if arg_list[2] in obj_dict.keys():
+            obj_dict[arg_list[2]] = type(obj_dict[arg_list[2]])(arg_list[3])
+        else:
+            obj_dict[arg_list[2]] = arg_list[3]
+        storage.save()
 
 
 if __name__ == '__main__':
