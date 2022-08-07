7df669dbf4b1d531653a2c43c76d0927ea45ccd5
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-08-07 11:15:21 -0700

    mod

--- models/user.py
+++ models/user.py
@@ -10,7 +10,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """ Initialize class instance with base class"""
-        super().__init__(self, *args, **kwargs)
